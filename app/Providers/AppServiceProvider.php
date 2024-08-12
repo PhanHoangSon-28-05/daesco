91fe7392000a7c5b479ba948980d7af0a813ca4c
PhanHoangSon-28-05 <hoangson28052002@gmail.com>
2024-08-13 00:16:27 +0700

    up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -69,6 +69,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //  \URL::forceScheme('https');
-        // Paginator::useBootstrap();
+        Paginator::useBootstrap();
     }
 }
